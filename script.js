3deafd1741271dcf7c6861aa75058d791d5f47dd
SWATHYMOL SAJEEV <swathysajeev35@gmail.com>
2024-12-06 20:42:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// COPYRIGHT (C) 2024 SWATHYMOL SAJEEV | GNU General Public License v3.0 
+
 for(let i = 1; i < 2000; i++){
     const shape = document.createElement("span");
     document.querySelector(".container").appendChild(shape);
